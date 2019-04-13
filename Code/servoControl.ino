715a66eaaa7df639350e8d77c937f9e81ffd093d
ThoreUndJonathan <42578047+ThoreUndJonathan@users.noreply.github.com>
2019-04-13 10:20:21 +0200

    Add files via upload

---
+++ Code/servoControl.ino
@@ -0,0 +1,56 @@
+#include <Keypad.h>
+#include <Servo.h>
+
+Servo bspServo;
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+int bspDegree = 90;
+
+byte rowPins[ROWS] = {2, 3, 4, 5}; 
+byte colPins[COLS] = {7, 8, 9, 10}; 
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void setup(){
+  Serial.begin(9600);
+  bspServo.attach(11);
+  bspServo.write(90);
+}
+  
+void loop(){
+  char Key = customKeypad.getKey();
+  
+  if(Key){
+    Serial.println(Key);
+    if(Key == '2'){
+      bspDegree = bspDegree + 5;
+      bspServo.write(bspDegree);
+    }
+    else if(Key == '8'){
+      bspDegree = bspDegree - 5;
+      bspServo.write(bspDegree);
+    }
+    else if(Key == '4'){
+      bspDegree = bspDegree + 1;
+      bspServo.write(bspDegree);
+    }
+    else if(Key == '6'){
+      bspDegree = bspDegree - 1;
+      bspServo.write(bspDegree);
+    }
+    else if(Key == 'A'){
+      bspDegree = 90;
+      bspServo.write(bspDegree);
+    }
+  }
+  delay(50);
+}
